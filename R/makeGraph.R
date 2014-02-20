10551860455f355a98d957a7e1e449079bce20be
Aaron Brooks <abrooks@systemsbiology.org>
2014-02-19 21:53:53 -0800

    stupid

--- R/makeGraph.R
+++ R/makeGraph.R
@@ -7,7 +7,6 @@ makeGraph <- function(n,chance,popularity,age) {
 	}
 	pa <- linearInterpolate(((1-(chance/100))*(popularity/100)),0,1,c(0,100))
 	aging <- linearInterpolate(((1-(chance/100))*(popularity/100)),0,-5)
-	print(pa.exp,aging.exp)
 	g <- aging.prefatt.game(n, pa.exp=pa,aging.exp=aging,directed=F)
 	V(g)$names <- seq(0,length(V(g))-1)
 	return(g)
