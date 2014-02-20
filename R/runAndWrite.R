74e472975e9e12c0a59225523d6f6ec7fa769fdc
Aaron Brooks <abrooks@systemsbiology.org>
2014-02-19 19:39:50 -0800

    return added

--- R/runAndWrite.R
+++ R/runAndWrite.R
@@ -2,4 +2,5 @@ runAndWrite <- function(n,chance,popularity,age,nRuns) {
 	g <- makeGraph(n,chance,popularity,age)
 	results <- runGame(g,nRuns)
 	results_score <- score(results,nRuns)
+	return(results_score)
 }
