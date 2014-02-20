e07ad1c8ed9e1823e7d59b5b24e16e6e91fa366b
Aaron Brooks <abrooks@systemsbiology.org>
2014-02-19 21:52:28 -0800

    update make graph

--- R/makeGraph.R
+++ R/makeGraph.R
@@ -5,12 +5,10 @@ makeGraph <- function(n,chance,popularity,age) {
 		interp <- approxfun(range,c(min,max))
 		return(interp(value))
 	}
-	pa.exp <- linearInterpolate(((1-(chance/100))*(popularity/100)),0,1,c(0,100))
-	aging.exp <- linearInterpolate(((1-(chance/100))*(popularity/100)),0,-5)
-	g <- aging.prefatt.game(n, pa.exp, aging.exp, m = NULL, aging.bin = 300,
-     out.dist = NULL, out.seq = NULL, out.pref = FALSE,
-     directed = FALSE, zero.deg.appeal = 1, zero.age.appeal = 0,
-     deg.coef = 1, age.coef = 1, time.window = NULL)
+	pa <- linearInterpolate(((1-(chance/100))*(popularity/100)),0,1,c(0,100))
+	aging <- linearInterpolate(((1-(chance/100))*(popularity/100)),0,-5)
+	print(pa.exp,aging.exp)
+	g <- aging.prefatt.game(n, pa.exp=pa,aging.exp=aging,directed=F)
 	V(g)$names <- seq(0,length(V(g))-1)
 	return(g)
 }
