350386c2deaeb9b8ce92f3c0056fe03fe33a423e
Aaron Brooks <abrooks@systemsbiology.org>
2014-02-25 15:09:30 -0800

    updated r fcns

--- R/makeGraph.R
+++ R/makeGraph.R
@@ -1,12 +1,12 @@
 makeGraph <- function(n,chance,popularity,age) {
 	require(igraph)
 	# chance acts on both parameters controlling 
-	linearInterpolate <- function(value,min,max,range=c(0,100)) {
+	linearInterpolate <- function(value,min,max,range=c(0,1)) {
 		interp <- approxfun(range,c(min,max))
 		return(interp(value))
 	}
-	pa <- linearInterpolate(((1-(chance/100))*(popularity/100)),0,1,c(0,100))
-	aging <- linearInterpolate(((1-(chance/100))*(popularity/100)),0,-5)
+	pa <- linearInterpolate(((1-(chance/100))*(popularity/100)),0,1)
+	aging <- linearInterpolate(((1-(chance/100))*(popularity/100)),-1,1)
 	g <- aging.prefatt.game(n, pa.exp=pa,aging.exp=aging,directed=F)
 	V(g)$names <- seq(0,length(V(g))-1)
 	return(g)
