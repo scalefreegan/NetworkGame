25dff3a9e2f80ee68f58235d7042b9bafe9484c2
Aaron Brooks <abrooks@systemsbiology.org>
2014-03-10 14:18:13 -0700

    ugh db!

--- R/run.R
+++ R/run.R
@@ -44,13 +44,12 @@ run <- function(n,chance,popularity,age,nRuns,uName,breaks) {
 		if(dbGetInfo(rs, what = "rowsAffected") > 1){
 			warning("Rolling back transaction")
 			dbRollback(con)
-		}else{
+		} else{
 			dbCommit(con)
-			dbDisconnect(con)
-			dbUnloadDriver(drv)
+			
 	}
-	# 
-	# 
+	dbDisconnect(con)
+	dbUnloadDriver(drv)
 	# dbGetQuery(con,"select * from networks")
 	# dbListFields(con,"networks")
 	# data for graph1
