d16c77914ae3a65228fe8933dfa582aa8423790e
Aaron Brooks <abrooks@systemsbiology.org>
2014-03-22 21:43:51 -0700

    lowered pop impt

--- R/makeGraph.R
+++ R/makeGraph.R
@@ -5,7 +5,7 @@ makeGraph <- function(n,chance,popularity,age) {
 		interp <- approxfun(range,c(min,max))
 		return(interp(value))
 	}
-	pa <- linearInterpolate(((1-(chance/100))*(popularity/100)),0,4)
+	pa <- linearInterpolate(((1-(chance/100))*(popularity/100)),0,1)
 	aging <- linearInterpolate(((1-(chance/100))*(age/100)),-2,2)
 	g <- aging.prefatt.game(n, pa.exp=pa,aging.exp=aging,directed=F)
 	V(g)$names <- seq(0,length(V(g))-1)
